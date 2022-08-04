f74e7c34153c8a0b50d93c93dc4b8313b35201c8
Abhishek Pandey <64667840+1abhishekpandey@users.noreply.github.com>
2022-08-04 21:18:26 +0530

    Added test cases

--- Examples/SampleAppSwift/SampleAppSwift/AppDelegate.swift
+++ Examples/SampleAppSwift/SampleAppSwift/AppDelegate.swift
@@ -21,39 +21,143 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
         let config: RSConfig = RSConfig(writeKey: "1wvsoF3Kx2SczQNlx1dvcqW9ODW")
             .dataPlaneURL("https://rudderstacz.dataplane.rudderstack.com")
             .loglevel(.debug)
-            .trackLifecycleEvents(true)
-            .recordScreenViews(true)
+            .trackLifecycleEvents(false)
+            .recordScreenViews(false)
         
         client = RSClient.sharedInstance()
         RSClient.sharedInstance().configure(with: config)
 
         client?.addDestination(RudderBrazeDestination())
-        
-        client?.track(RSEvents.Ecommerce.orderCompleted, properties: [
-            "key-1": "value-1",
-            "key-2": 2,
-            RSKeys.Ecommerce.revenue: 5007,
-            "products": [
-                [
-                    RSKeys.Ecommerce.productId: "Pro-1001",
-                    RSKeys.Ecommerce.quantity: 1,
-                    RSKeys.Ecommerce.price: 1001.11
-                ],
-                [
-                    RSKeys.Ecommerce.productId: "Pro-2002",
-                    RSKeys.Ecommerce.quantity: 2,
-                    RSKeys.Ecommerce.price: 2002.22,
-                    "pro-key-1": "pro-value-1"
-                ]
-            ]
-        ])
-        
-        // Register for push notifications: https://www.braze.com/docs/developer_guide/platform_integration_guides/ios/push_notifications/integration/#step-3-register-for-push-notifications
-        setupPushCategories()
+        client?.track("Last event")
+//        setupPushCategories()
+//        sendEvents()
         return true
     }
     
+    func sendEvents() {
+        let client = RSClient.sharedInstance()
+        
+        identify()
+        func identify() {
+            let option = RSOption()
+            option.putExternalId("brazeExternalId", withId: "ID-1")
+            client.identify("User swift iOS 1", traits: [
+                RSKeys.Identify.Traits.firstName: "firstName",
+                RSKeys.Identify.Traits.lastName: "lastName",
+                RSKeys.Identify.Traits.email: "test@mail.com",
+                RSKeys.Identify.Traits.birthday: "2016-12-14T13:32:31.601",
+                RSKeys.Identify.Traits.gender: "MaLe",
+                RSKeys.Identify.Traits.phone: "0123456789",
+                RSKeys.Identify.Traits.address: [
+                    RSKeys.Identify.Traits.Address.city: "City value",
+                    RSKeys.Identify.Traits.Address.country: "India"
+                ],
+                "Traits key-1": "Traits value-1"
+            ], option: option)
+        }
+        
+        let emptyProducts: [String: Any] = [
+            RSKeys.Ecommerce.products: [[:]]
+        ]
+        let products: [[String: Any]] = [
+            [
+                RSKeys.Ecommerce.productId: "1003",
+                RSKeys.Ecommerce.quantity: 13,
+                RSKeys.Ecommerce.price: 100.33,
+                "Product-Key-13": "Product-Value-13"
+            ]
+        ]
+        let twoProducts: [[String: Any]] = [
+            [
+                RSKeys.Ecommerce.productId: "1006",
+                RSKeys.Ecommerce.quantity: 16,
+                RSKeys.Ecommerce.price: 100.66,
+                "Product-Key-16": "Product-Value-16"
+            ],
+            [
+                RSKeys.Ecommerce.productId: "2006",
+                RSKeys.Ecommerce.quantity: 26,
+                RSKeys.Ecommerce.price: 200.66,
+                "Product-Key-26": "Product-Value-26"
+            ]
+        ]
+        let propertiesWithProductsAndWithoutCustomProperty: [String: Any] = [
+            RSKeys.Ecommerce.products: [[
+                RSKeys.Ecommerce.productId: "1004",
+                RSKeys.Ecommerce.quantity: 14,
+                RSKeys.Ecommerce.price: 100.44
+            ]],
+            RSKeys.Ecommerce.currency: "INR"
+        ]
+        let propertiesWithTwoProductsAndCustomProperty: [String: Any] = [
+            RSKeys.Ecommerce.products: twoProducts,
+            RSKeys.Ecommerce.revenue: 126,
+            RSKeys.Ecommerce.currency: "INR",
+            "Key-16": "Value-16"
+        ]
+        let propertiesWithProductsAndCustomProperty: [String: Any] = [
+            RSKeys.Ecommerce.products: products,
+            RSKeys.Ecommerce.revenue: 1233,
+            RSKeys.Ecommerce.currency: "INR",
+            "Key-13": "Value-13"
+        ]
+        let propertiesWithoutProducts: [String: Any] = [
+            RSKeys.Ecommerce.productId: "1007",
+            RSKeys.Ecommerce.quantity: 17,
+            RSKeys.Ecommerce.price: 100.77,
+            RSKeys.Ecommerce.revenue: 127,
+            RSKeys.Ecommerce.currency: "INR",
+            "Key-17": "Value-17"
+        ]
+        let customProperties: [String: Any] = [
+            "Key-1": emptyProducts,
+            "Key-2": 123,
+            "Key-3": "INR",
+            "Key-4": 10005.1,
+            "Key-5": "Value-1"
+        ]
+        track()
+        func track() {
+            // Without Campaign properties
+            client.track("Install Attributed")
+            // With Campaign properties
+            client.track("Install Attributed", properties: [
+                "campaign": [
+                    "source": "Source value",
+                    "name": "Name value",
+                    "ad_group": "ad_group value",
+                    "ad_creative": "ad_creative value"
+                ]
+            ])
+            
+            // Revenue event
+            client.track("Random Revenue event", properties: [
+                RSKeys.Ecommerce.revenue: 123
+            ])
+            client.track(RSEvents.Ecommerce.orderCompleted)   // No event will be sent as property is empty
+            // With only empty Products
+            client.track(RSEvents.Ecommerce.orderCompleted, properties: emptyProducts)    // No event should be made
+            
+            // With Products
+            client.track(RSEvents.Ecommerce.orderCompleted, properties: propertiesWithProductsAndCustomProperty)
+            client.track(RSEvents.Ecommerce.orderCompleted, properties: propertiesWithProductsAndWithoutCustomProperty)
+            client.track("Ecomm track events", properties: propertiesWithProductsAndCustomProperty)
+            client.track("Ecomm track events without revenue", properties: propertiesWithProductsAndWithoutCustomProperty)  // Custom event
+            
+            client.track(RSEvents.Ecommerce.orderCompleted, properties: propertiesWithTwoProductsAndCustomProperty)
+            client.track("With two products", properties: propertiesWithTwoProductsAndCustomProperty)
+            // Without Products
+            client.track(RSEvents.Ecommerce.orderCompleted, properties: propertiesWithoutProducts)
+            // Custom events with properties
+            client.track("Custom track event_1", properties: customProperties)
+            // Custom events without properties
+            client.track("Custom track event_3")
+            
+        }
+    }
+    
     func setupPushCategories() {
+        // Register for push notifications: https://www.braze.com/docs/developer_guide/platform_integration_guides/ios/push_notifications/integration/#step-3-register-for-push-notifications
         if #available(iOS 10, *) {
           let center = UNUserNotificationCenter.current()
           center.delegate = self
@@ -66,8 +170,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
           }
           UIApplication.shared.registerForRemoteNotifications()
         } else {
-          let types : UIUserNotificationType = [.alert, .badge, .sound]
-          let setting : UIUserNotificationSettings = UIUserNotificationSettings(types:types, categories:nil)
+          let types: UIUserNotificationType = [.alert, .badge, .sound]
+          let setting: UIUserNotificationSettings = UIUserNotificationSettings(types: types, categories: nil)
           UIApplication.shared.registerUserNotificationSettings(setting)
           UIApplication.shared.registerForRemoteNotifications()
         }
@@ -86,7 +190,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
         RSClient.sharedInstance().userNotificationCenter(center, didReceive: response, withCompletionHandler: completionHandler)
     }
     
-
     // MARK: UISceneSession Lifecycle
 
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
